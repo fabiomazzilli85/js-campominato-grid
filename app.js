b1ca8222987ea40bb8acfa130030fcd499b62352
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-03-13 17:38:42 +0100

    Aggiunta classe CSS. Aggiunto evento listerner.

--- app.js
+++ app.js
@@ -2,20 +2,26 @@ document.addEventListener("DOMContentLoaded", function() {
     const container = document.querySelector('.container'); 
     const showButton = document.getElementById('showButton');
     
-    
     // Creo 100 div che vanno a sostituire il markup HTML.
     for (let i = 1; i <= 100; i++) {
         const div = document.createElement('div');
         div.classList.add('box', 'hidden'); // Aggiungo, oltre alla classe `box`, che ha le sue proprietà CSS, la classe 'hidden' per nascondere i div (utile successiivamente).
-        div.textContent = i;
+        div.textContent = i; 
+        div.addEventListener('click', function() { // Qui ho fatto molto copia e incolla ma  la lettura del codice mi pare abbastanza chiara.
+ 
+            document.querySelectorAll('.box').forEach(box => {
+                box.classList.remove('clicked'); // Qui rimuovo la classe `clicked` a tutti i box.
+            });
+ 
+            this.classList.add('clicked'); // Qui, invece, aggiungo la classe agli elementi su cui clicco. ''This'' lo abbiamo visto oggi con Gianliuca.
+        });
         container.appendChild(div);
     }
-    
     
     // Aggiungo un listener al bottone.
     showButton.addEventListener('click', function() {
         
-        // Seleziono tutti i div creati.
+
         const boxes = document.querySelectorAll('.box');
         
         // Rimuovo la classe 'hidden' per mostrare i div.
@@ -23,7 +29,7 @@ document.addEventListener("DOMContentLoaded", function() {
             box.classList.remove('hidden');
         });
         
-        // Nascondo il bottone quando viene cliccato.
+        // Nascondo il bottone dopo averci  cliccato.
         showButton.style.display = 'none';
     });
 });
