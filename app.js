c8fa070ce4d0c694f5f2241b3a38aa639b53465a
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-03-13 17:43:38 +0100

    Aggiunta frase in console.log.

--- app.js
+++ app.js
@@ -8,12 +8,14 @@ document.addEventListener("DOMContentLoaded", function() {
         div.classList.add('box', 'hidden'); // Aggiungo, oltre alla classe `box`, che ha le sue proprietà CSS, la classe 'hidden' per nascondere i div (utile successiivamente).
         div.textContent = i; 
         div.addEventListener('click', function() { // Qui ho fatto molto copia e incolla ma  la lettura del codice mi pare abbastanza chiara.
- 
+            
             document.querySelectorAll('.box').forEach(box => {
                 box.classList.remove('clicked'); // Qui rimuovo la classe `clicked` a tutti i box.
             });
- 
+            
             this.classList.add('clicked'); // Qui, invece, aggiungo la classe agli elementi su cui clicco. ''This'' lo abbiamo visto oggi con Gianliuca.
+            const clickedNumber = parseInt(this.textContent);
+            console.log("Hai cliccato sul div numero:", clickedNumber); // Qui mando in console la frase `Hai cliccaro sul duv numero` e il `clickedNumber`.
         });
         container.appendChild(div);
     }
@@ -21,7 +23,7 @@ document.addEventListener("DOMContentLoaded", function() {
     // Aggiungo un listener al bottone.
     showButton.addEventListener('click', function() {
         
-
+        
         const boxes = document.querySelectorAll('.box');
         
         // Rimuovo la classe 'hidden' per mostrare i div.
