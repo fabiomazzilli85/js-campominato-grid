e1fbc95a51625289533767b3870ddbb14c7b89a1
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-03-13 14:37:19 +0100

    Modificato console.log.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const hideElement = document.getElementById("hide");
 
 // Al listener del bottone, la classe `d-none` è rimossa e il contenuto della pagina è correttamente visualizzato.
 bottone.addEventListener('click', function() {
-    console.log("Il bottone funziona."); // Uso il console.log per capire se il listener funziona a dovere.
+    console.log("Il bottone funziona e dovresti visualizzare una tabella."); // Uso il console.log per capire se il listener funziona a dovere.
     hide.classList.remove("d-none");
 });
 
