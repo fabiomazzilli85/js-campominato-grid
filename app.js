7e5925950ef477fca68440b624b41bc89bd58e83
fabiomazzilli85 <fabio_mazzilli@outlook.it>
2024-03-13 15:54:01 +0100

    Vari bug-fix. Flexbox non funziona corrrettamente

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const container = document.getElementById("box2");
+    const container = document.getElementById("table");
     
     for (let i = 1; i <= 100; i++) {
         const div = document.createElement("div");
-        div.className = "item padding-box";
+        div.className = "item box padding-box";
         div.textContent = i;
         container.appendChild(div);
     }
 });
 
-
 // Richiamo i due elementi della pagina con id `button` e `hide`
 const bottone = document.getElementById("button");
 const hideElement = document.getElementById("hide");
 
-// Al listener del bottone, la classe `d-none` è rimossa e il contenuto della pagina è correttamente visualizzato.
 bottone.addEventListener('click', function() {
-    console.log("Il bottone funziona e dovresti visualizzare una tabella."); // Uso il console.log per capire se il listener funziona a dovere.
-    hide.classList.remove("d-none");
-});
+    console.log("Il bottone è stato cliccato, ottimo!"); // Visualizza un messaggio di log nella console del browser
 
+});
